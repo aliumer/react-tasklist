de5c7e5a8eb01ebcc6374f1ce9ec9362a49b19f4
Muhammad Ali Umer <muhammad.ali.umber@directlinegroup.co.uk>
2020-03-16 11:31:08 +0000

    added async await and properties implementation.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import TaskList from "./taskList";
 import './index.css';  
+
+class App extends React.Component {
+  state = {
+    answer: 43
+  };
+
+  asyncfunction = () => {
+    return Promise.resolve(37);
+  };
+
+  async componentDidMount() {
+    this.setState({
+      answer: await this.asyncfunction()
+    });
+  }
+
+  render(){
+    return (
+    <h3>Test {this.state.answer}</h3>
+    );
+  }
+}
+export default App;
   
   ReactDOM.render(
-    <TaskList />,
+    <App />,
     document.getElementById('root')
   );
   
